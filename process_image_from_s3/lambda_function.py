89abc74def9cfc758d3b45b399ca8df6067ef4b9
Fabio Zanetti <fczanetti@hotmail.com>
2025-03-09 18:01:34 -0300

    Improve lambda

--- process_image_from_s3/lambda_function.py
+++ process_image_from_s3/lambda_function.py
@@ -3,18 +3,21 @@ import boto3
 from datetime import datetime
 import base64
 import os
+import json
 
 
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=os.environ.get("AWS_ACCESS_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS"))
-bucket_name = os.environ.get("AWS_BUCKET_NAME")
-bucket = s3.Bucket(bucket_name)
+# s3 = boto3.resource(
+#     "s3",
+#     aws_access_key_id=os.environ.get("AWS_ACCESS_ID"),
+#     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS"))
+# bucket_name = os.environ.get("AWS_BUCKET_NAME")
+# bucket = s3.Bucket(bucket_name)
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     log.info("Starting lambda to process image from S3.")
-    print(event)
+    body = json.loads(event["Records"][0]["body"])
+    key = body["Records"][0]["s3"]["object"]["key"]
+    print(key)
